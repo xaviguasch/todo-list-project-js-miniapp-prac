f78cf090a9fefc272ce105f039e6f4fd429a862c
xaviguasch <xaviguasch@gmail.com>
2020-12-15 18:03:40 +0100

    added minor tweaks

--- todos.js
+++ todos.js
@@ -1,24 +1,30 @@
-let result = ''
+let input = ''
 let todos = []
 
-while (result !== 'quit') {
-  result = prompt('What would you like to do?')
+while (input !== 'quit' && input !== 'q') {
+  input = prompt('What would you like to do?')
 
-  if (result === 'new') {
-    let newTodo = prompt('Enter new todo')
+  if (input === 'new') {
+    const newTodo = prompt('Enter new todo')
     console.log(`${newTodo} added to list`)
     todos.push(newTodo)
-  } else if (result === 'list') {
+  } else if (input === 'list') {
     console.log('**********')
     todos.forEach((todo, i) => {
       console.log(`${i}: ${todo}`)
     })
     console.log('**********')
-  } else if (result === 'delete') {
-    let toDeleteIndex = prompt('Enter a todo to delete')
-    todos.splice(toDeleteIndex, 1)
-    console.log('todo removed')
+  } else if (input === 'delete') {
+    const toDeleteIndex = parseInt(prompt('Enter a todo to delete'))
+    if (!Number.isNaN(toDeleteIndex)) {
+      todos.splice(toDeleteIndex, 1)
+      console.log('todo removed')
+    } else {
+      console.log('Unknown Index')
+    }
   } else {
     console.log('Pick a valid command!')
   }
 }
+
+console.log('OK, you quit the app!!!')
