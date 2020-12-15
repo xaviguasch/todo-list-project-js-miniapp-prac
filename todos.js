1f2255f6d35f603b0a1bad75d95643586688ce83
xaviguasch <xaviguasch@gmail.com>
2020-12-15 16:50:12 +0100

    added solution

--- todos.js
+++ todos.js
@@ -1,1 +1,24 @@
-console.log('hey')
+let result = ''
+let todos = []
+
+while (result !== 'quit') {
+  result = prompt('What would you like to do?')
+
+  if (result === 'new') {
+    let newTodo = prompt('Enter new todo')
+    console.log(`${newTodo} added to list`)
+    todos.push(newTodo)
+  } else if (result === 'list') {
+    console.log('**********')
+    todos.forEach((todo, i) => {
+      console.log(`${i}: ${todo}`)
+    })
+    console.log('**********')
+  } else if (result === 'delete') {
+    let toDeleteIndex = prompt('Enter a todo to delete')
+    todos.splice(toDeleteIndex, 1)
+    console.log('todo removed')
+  } else {
+    console.log('Pick a valid command!')
+  }
+}
